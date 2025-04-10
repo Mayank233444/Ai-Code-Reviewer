2f1f06d29b6cd8eaded1268922c6f0b0bbd0e967
Nischay Sinha <nischay.sinha.che23@itbhu.ac.in>
2025-04-11 01:20:50 +0530

    basic changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 
 
